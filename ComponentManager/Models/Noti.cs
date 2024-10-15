f184cc0576639b82d1d6dd49edaffdeeadb69d30
Hiep Dang Duc <24182@sews.local>
2024-10-15 19:08:22 +0700

    sql

--- ComponentManager/Models/Noti.cs
+++ ComponentManager/Models/Noti.cs
@@ -14,6 +14,8 @@ namespace ComponentManager.Models
         [Key]
         public int Id { get; set; }
         public string Username { get; set; }
+        public string ComponentName { get; set; }
+        public string MyProperty { get; set; }
         public string NotiType { get; set; }
         public int Quantity { get; set; }
         public bool IsHandled { get; set; } = false;
